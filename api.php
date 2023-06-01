2dff619b078d86491a3ea9dafd6f4fb5aa359f22
JuanJoseDuranRinconCAMPUS2 <jduranrinconcampus@gmail.com>
2023-06-01 17:47:30 -0500

    feat: :sparkles: Sistema de guardado de numeros completado
    se realiza el sistema para que guarde localmente los numeros a calcular

--- api.php
+++ api.php
@@ -1,35 +1,45 @@
 <?php
     session_start();
-    if (!isset($_SESSION['sistema'])) {
-        $_SESSION['sistema'] = '';
-};
+    if (!isset($_SESSION['Numeros'])) {
+        $_SESSION['Numeros'] = '';
+        $_SESSION['valor1'] = '';
+        $_SESSION['valor2'] = '';
 
-if($_SERVER["REQUEST_METHOD"] == "POST"){
-    if (isset($_POST["numero"])) {
-        $numero = $_POST["numero"];
-        echo $numero;
-        $_SESSION['sistema'] .= $numero;
+    };
+
+    $num1 = '';
+
+    if($_SERVER["REQUEST_METHOD"] == "POST"){
+        if (isset($_POST["numero"])) {
+            $numero = $_POST["numero"];
+            echo $numero;
+            $_SESSION['Numeros'] .= $numero;
+        
+        }
+        if (isset($_POST["operacion"])) {
+            $_SESSION['valor1'] = $_SESSION['Numeros'];
+            $operation = $_POST["operacion"];
+            $_SESSION['Numeros'] = '';
+        }
+        if(isset($_POST["Resultado"])){
+            $_SESSION['valor2'] = $_SESSION['Numeros'];
+            $_SESSION['Numeros'] = '';
+
+            
+        }
+    };
+    if(isset($_POST["borrar"])){
+        $_SESSION['Numeros'] = '';
+        $_SESSION['valor1'] = '';
+        $_SESSION['valor2'] = '';
+    }
     
-    }
-    if (isset($_POST["operation"])) {
-        $operation = $_POST["operation"];
-        if (isset($operation)) {
-            $_SESSION['sistema'] .= $operation;
-        }
-    }
-    if (isset($_POST["Resultado"])) {
-        $numeros = $_SESSION['sistema'];
-        if (isset($numeros)) {
-            $resultado = eval("return $numeros;");
-            $_SESSION['sistema'] = $resultado;
-
-        }
-    }
-};
-if(isset($_POST["borrar"])){
-    $_SESSION['sistema'] = '';
-}
-var_dump ($_SESSION['sistema']);
+    echo "data ingresada <br>";
+    var_dump ($_SESSION['Numeros']);
+    echo "<br> valor 1 <br>";
+    var_dump ($_SESSION['valor1']);
+    echo "<br> valor 2 <br>";
+    var_dump ($_SESSION['valor2']);
 ?>
 
 <!DOCTYPE html>
@@ -43,35 +53,35 @@ var_dump ($_SESSION['sistema']);
 <body>
     <!-- idea: los botones van a insertar data en el input "pantalla" esto evitando problemas con la recarga de data-->
     <form action="api.php" method="POST" id="calculator">
-        <input type="text" class="pantalla" name="inputNumerico" value="<?php  echo  isset($_SESSION['sistema']) ? $_SESSION['sistema'] : '0'; ?>"  readonly>
+        <input type="text" class="pantalla" name="inputNumerico" value="<?php  echo  isset($_SESSION['Numeros']) ? $_SESSION['Numeros'] : '0'; ?>"  readonly>
         <br>
         <table>
             <tr>
                 <td><button type="submit" name="numero" value="7"> 7 </button></td>
                 <td><button type="submit" name="numero" value="8"> 8 </button></td>
                 <td><button type="submit" name="numero" value="9"> 9 </button></td>
-                <td><button type="submit" name="operation" value="/"> % </button></td>
+                <td><button type="submit" name="operacion" value="/"> % </button></td>
                 
             </tr>
             <tr>
                 <td><button type="submit" name="numero" value="4"> 4 </button></td>
                 <td><button type="submit" name="numero" value="5"> 5 </button></td>
                 <td><button type="submit" name="numero" value="6"> 6 </button></td>
-                <td><button type="submit" name="operation" value="*"> * </button></td>
+                <td><button type="submit" name="operacion" value="*"> * </button></td>
                 
             </tr>
             <tr>
                 <td><button type="submit" name="numero" value="1"> 1 </button></td>
                 <td><button type="submit" name="numero" value="2"> 2 </button></td>
                 <td><button type="submit" name="numero" value="3"> 3 </button></td>
-                <td><button type="submit" name="operation" value="-"> - </button></td>
+                <td><button type="submit" name="operacion" value="-"> - </button></td>
 
             </tr>
             <tr>
                 <td><button type="submit" name="numero" value="0"> 0 </button></td>
                 <td><button type="submit" name="numero" value="."> . </button></td>
                 <td><button type="submit" name="Resultado" value="="> = </button></td>
-                <td><button type="submit" name="operation" value="+"> + </button></td>
+                <td><button type="submit" name="operacion" value="+"> + </button></td>
             </tr>
             <tr>
                 <td><button type="submit" name="borrar" value="C">  C </button></td>
